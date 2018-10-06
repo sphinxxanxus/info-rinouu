612e2a98b1c2ce7a46fc8fedfb53e2a16ed97bf0
rinouu <43865347+rinouu@users.noreply.github.com>
2018-10-07 00:43:00 +0100

    Add files via upload

---
+++ exercice0.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"\t I'm Corine, an active member of Ceceonat!" << endl;
+    cout <<"\t Email: corinedovonou@gmail.com" <<endl;
+    cout <<"\t Logins:" <<endl;
+    cout <<"\t\t Github: rinouu" <<endl;
+    cout <<"\t\t Gitlab: rine" <<endl;
+    cout <<"\t Programming Languages: C++, Java, C" <<endl;
+    return 0;
+}
